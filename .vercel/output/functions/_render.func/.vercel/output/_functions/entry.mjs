d506b20d8d89ec5f36798faaec33606ca3940164
Pablo Cesar Torres GIlse <pablocesar.t.g@gmail.com>
2026-07-05 00:06:15 -0500

    fix: remove sessionStorage so it shows every time

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DMbeASer.mjs';
-import { manifest } from './manifest_C1e6uqZJ.mjs';
+import { manifest } from './manifest_BLwrLBDX.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/en.astro.mjs');
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "83f731de-9f00-46e6-be62-ec113c9d3abe",
+    "middlewareSecret": "3a6c8930-ba52-4468-af15-13e378eb491f",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
